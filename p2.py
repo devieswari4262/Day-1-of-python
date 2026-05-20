53df22a9f8fdd093070f84d24e3b53c7f2ab473d
devi eswari <vanapallidevieswari@gmail.com>
2026-05-20 19:56:06 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,62 @@
+#Define the variables
+
+'''a=10
+print(a)''' 
+# assign the values for the variable
+
+#Limitations
+'''3b=20
+print(3b)'''
+#This  an  error because to run the code no number will be the first at variable
+
+'''@c=45
+print(@c)'''
+#This gives an syntax error because character 
+
+'''student age(25)
+print(student age)'''
+#this is invalid because there is a sapce between the variable to variable
+
+'''andhra_pradesh=20
+print(andhra_pradesh)'''
+#This is valid because we use the limtation of variable underscore
+
+'''a=10,20,30
+print(a)'''
+#it can hold the multiple values
+
+'''a=10
+b=20
+c=30
+print(a,b,c)'''
+#it can store the values in variables
+
+'''a=10
+a=20
+a=30
+print(a)'''
+#it is caannot hold the multiple values because it's stores single value
+
+'''a=10
+a=20
+print(a)
+a=30'''
+#This variable can store upto 20 because string inserted between values
+
+'''a='AP is a capital of "Amaravathi"' 
+print(a)'''
+#This is an represent the sepical words in string
+ 
+c=chennai
+print(c)
+#This is invalid because combination characterstics must be in strings
+
+'''d="hyderbad"
+print(d)'''
+#This is valid 
+
+
+
+
+
+
